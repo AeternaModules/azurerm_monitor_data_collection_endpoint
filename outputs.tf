211b4939670039df051f1b0728e1574af4d7aba6
Daniel Allen <dan@aeternagroup.net>
2026-01-11 16:43:47 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "monitor_data_collection_endpoints" {
+  description = "All monitor_data_collection_endpoint resources"
+  value       = azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints
+}
+
