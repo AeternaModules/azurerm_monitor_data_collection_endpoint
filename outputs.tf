2887e0d23deac6fb27bbc6f9daf7325918fb1bcc
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:49:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,49 @@
 output "monitor_data_collection_endpoints_id" {
   description = "Map of id values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.id }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "monitor_data_collection_endpoints_configuration_access_endpoint" {
   description = "Map of configuration_access_endpoint values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.configuration_access_endpoint }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.configuration_access_endpoint if v.configuration_access_endpoint != null && length(v.configuration_access_endpoint) > 0 }
 }
 output "monitor_data_collection_endpoints_description" {
   description = "Map of description values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.description }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.description if v.description != null && length(v.description) > 0 }
 }
 output "monitor_data_collection_endpoints_immutable_id" {
   description = "Map of immutable_id values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.immutable_id }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.immutable_id if v.immutable_id != null && length(v.immutable_id) > 0 }
 }
 output "monitor_data_collection_endpoints_kind" {
   description = "Map of kind values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.kind }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.kind if v.kind != null && length(v.kind) > 0 }
 }
 output "monitor_data_collection_endpoints_location" {
   description = "Map of location values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.location }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "monitor_data_collection_endpoints_logs_ingestion_endpoint" {
   description = "Map of logs_ingestion_endpoint values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.logs_ingestion_endpoint }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.logs_ingestion_endpoint if v.logs_ingestion_endpoint != null && length(v.logs_ingestion_endpoint) > 0 }
 }
 output "monitor_data_collection_endpoints_metrics_ingestion_endpoint" {
   description = "Map of metrics_ingestion_endpoint values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.metrics_ingestion_endpoint }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.metrics_ingestion_endpoint if v.metrics_ingestion_endpoint != null && length(v.metrics_ingestion_endpoint) > 0 }
 }
 output "monitor_data_collection_endpoints_name" {
   description = "Map of name values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.name }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "monitor_data_collection_endpoints_public_network_access_enabled" {
   description = "Map of public_network_access_enabled values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.public_network_access_enabled }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.public_network_access_enabled if v.public_network_access_enabled != null }
 }
 output "monitor_data_collection_endpoints_resource_group_name" {
   description = "Map of resource_group_name values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.resource_group_name }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "monitor_data_collection_endpoints_tags" {
   description = "Map of tags values across all monitor_data_collection_endpoints, keyed the same as var.monitor_data_collection_endpoints"
-  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.tags }
+  value       = { for k, v in azurerm_monitor_data_collection_endpoint.monitor_data_collection_endpoints : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
